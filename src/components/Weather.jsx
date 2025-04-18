3043d9abd4763ef4e98f51a10b58073e5ef06dd6
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2025-04-18 20:00:37 +0000

    data fixed

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router";
 
 function Weather({ data }) {
-  // const { temperature, description, icon } = data;
-  // const { forecastData } = data;
+  const { message, forecast, city } = data;
+
   console.log("in Weather: data", data);
   // console.log("in Weather: forecastData", forecastData);
   // const [responseMessage, setResponseMessage] = useState("");
 
   return (
     <div>
-      <h1>Weather Forecast</h1>
+      <h1>Weather Forecast for {city}</h1>
       {forecastData && <p>Forecast: {forecastData}</p>}
     </div>
   );
