d5bf8312a3e2399fc255eb561f4ccb7c74ec7c0c
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2025-04-18 16:33:31 +0000

    router dep added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router";
 import "./App.css";
 import HomePage from "./components/HomePage";
 // function App() {
