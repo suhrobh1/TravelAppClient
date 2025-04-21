61f2318cdcac201158e33870e7a72ab99534805e
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2025-04-20 21:21:00 -0700

    poi page updated

--- src/components/PointsInterest.jsx
+++ src/components/PointsInterest.jsx
@@ -4,6 +4,9 @@ import { Routes, Route } from "react-router";
 function PointsInterest({ data }) {
   const { message, microserviceData, city } = data;
 
+  const poiData = microserviceData.forecast;
+
+
   console.log("in PointsInterest: data", data);
   console.log("in PointsInterest: microserviceData", microserviceData);
   // const [responseMessage, setResponseMessage] = useState("");
@@ -12,17 +15,30 @@ function PointsInterest({ data }) {
 
   return (
     <div>
-    <h3 style={{ fontFamily: 'Arial, sans-serif' }}>PointsInterest near {city}</h3>
+    <h3 style={{ fontFamily: 'Arial, sans-serif' }}>Points Interest near {city}</h3>
     <table style={{ borderCollapse: 'collapse', width: '100%', fontFamily: 'Arial, sans-serif' }}>
       <thead>
         <tr>
-          <th style={{ border: '1px solid #ddd', padding: '8px', backgroundColor: '#f2f2f2' }}>Place</th>
-          <th style={{ border: '1px solid #ddd', padding: '8px', backgroundColor: '#f2f2f2' }}>Type</th>
-          <th style={{ border: '1px solid #ddd', padding: '8px', backgroundColor: '#f2f2f2' }}>Rate</th>
+          <th style={{ border: '1px solid #ddd', padding: '8px', backgroundColor: '#f2f2f2' }}>Name</th>
+          <th style={{ border: '1px solid #ddd', padding: '8px', backgroundColor: '#f2f2f2' }}>Catergory</th>
+          <th style={{ border: '1px solid #ddd', padding: '8px', backgroundColor: '#f2f2f2' }}>Address</th>
         </tr>
       </thead>
       <tbody>
-       
+      {poiData.places.map((day, index) => (
+          <tr key={index} style={{ backgroundColor: index % 2 === 0 ? '#ffffff' : '#f9f9f9' }}>
+            <td style={{ border: '1px solid #ddd', padding: '8px' }}>{day.time}</td>
+            <td style={{ border: '1px solid #ddd', padding: '8px' }}>
+              {day.minTemperature}°F - {day.maxTemperature}°F
+            </td>
+            <td style={{ border: '1px solid #ddd', padding: '8px' }}>
+              {day.precipitationProbability}{day.precipitationProbabilityUnit}
+            </td>
+            <td style={{ border: '1px solid #ddd', padding: '8px' }}>
+              {skyStatus(day.cloudCover)}
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   </div>
